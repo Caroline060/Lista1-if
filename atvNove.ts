33fea2d587254c3743f6f34dcae296d64ac95954
Caroline060 <161760019+Caroline060@users.noreply.github.com>
2024-03-07 13:54:29 -0300

    Add files via upload

---
+++ atvNove.ts
@@ -0,0 +1,21 @@
+/* (15 pontos)
+9- Para doar sangue é necessário ter entre 18 e 67 anos e possuir mais de 60Kg. Faça
+um aplicativo que pergunte a idade de uma pessoa e seu peso e diga se ela pode
+doar sangue ou não, caso não diga o(s) motivo(s).
+Nome: Nathália Caroline Gumbio
+*/
+
+
+const teclado = require (`prompt-sync`)();
+
+let idade: number = parseFloat(teclado(`Digite sua idade: `));
+let peso: number = parseFloat(teclado(`Digite quanto você pesa: `));
+
+if(idade >=18 && idade <=67){
+    if(peso >60){
+        console.log(`Você pode doar sangue!`)
+    }
+}
+else{
+    console.log(`Você não pode doar sangue!`)
+}
